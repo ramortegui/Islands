4e1b5ffe562162ef93622ae69d06a644dd115a18
Ruben Amortegui <ruben.amortegui@gmail.com>
2017-07-24 21:41:45 -0600

    Add first state :player_set

--- lib/islands_engine/rules.ex
+++ lib/islands_engine/rules.ex
@@ -1,8 +1,11 @@
 defmodule IslandsEngine.Rules do
   alias __MODULE__
-  defstruct state :inizialized
+  defstruct state: :initialized
   def new(), do: %Rules{}
 
+  def check( %Rules{state: :initialized } = rules, :add_player) do
+    {:ok, %Rules{ rules | state: :player_set }}
+  end
 
   def check( _state, _action ), do: :error
 end
